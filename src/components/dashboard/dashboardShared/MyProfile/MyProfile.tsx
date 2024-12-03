ea9d99ff714df7a87321fac687f5a6e7ac4648fe
Abdur Rahim <rahim11500000@gmail.com>
2024-12-03 23:23:45 +0600

    added demo imag

--- src/components/dashboard/dashboardShared/MyProfile/MyProfile.tsx
+++ src/components/dashboard/dashboardShared/MyProfile/MyProfile.tsx
@@ -17,7 +17,7 @@ export default function MyProfile() {
         <div className="text-center bg-gradient-to-br from-teal-500 to-[#0060f0] bg-opacity-20 rounded-lg p-6">
             <div className="avatar">
                 <div className="w-24 rounded-full">
-                    <img src={user?.image} />
+                    <img src={"/boy.png"} />
                 </div>
             </div>
             <h2 className="font-bold text-2xl text-white">Name: {user?.name}</h2>
