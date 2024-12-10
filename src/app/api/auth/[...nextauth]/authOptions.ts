c939d0c6198d44a1c66dd4ad4414f9a5cec6b672
Abdur Rahim <rahim11500000@gmail.com>
2024-12-10 22:46:51 +0600

    added middleware

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,9 +1,14 @@
-
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 // import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,17 +1,7 @@
 import SignIn from '@/components/SignIn/SignIn'
-import { jwtDecode } from 'jwt-decode';
-import { cookies } from 'next/headers';
 import React from 'react'
 
 export default async function page() {
-  const cookieStore = cookies();
-  const token = (await cookieStore).get("next-auth.session-token")?.value;
-  if (token) {
-    const decoded = await jwtDecode(token);
-    console.log(decoded);
-  }
-
-
   return (
     <div>
       <SignIn />
